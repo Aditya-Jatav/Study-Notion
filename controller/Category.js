3563a4fa749b852a97bdbcd1f03e95de0b2f4fcc
ADITYA JATAV <adityajatav19072004@gmail.com>
2024-06-16 17:08:57 +0530

    category controller done as day-6

--- controller/Category.js
+++ controller/Category.js
@@ -1,3 +1,4 @@
+const { validatePaymentVerification } = require("razorpay/dist/utils/razorpay-utils");
 const Category = require("../models/category");
 
 // create Category ki api or handler function
@@ -51,5 +52,49 @@ exports.showAllcategory = async(res,req)=>{
             success: false,
             message: error.message,
           })  
+    }
+}
+
+// category pageDetails
+exports.categoryPageDetails = async (req, res)=>{
+    try{
+        // get category
+        const {categoryId} = req.body;
+        // get courses for specified categoryId
+        const selectCategory = await Category.findById(categoryId)
+                                            .populate("courses")
+                                            .exec();
+        
+        // validatation
+        if(!selectCategory){
+            return res.status(404).json({
+                success: false,
+                message:'Data Not Found',
+            });
+        }  
+        // get courses for different categories
+        const differentCategories = await Category.find({
+                                              _id:{$ne: categoryId},
+                                             })   
+                                             .populate("courses")
+                                             .exec();
+                                             
+        // get top selling courses
+
+        // return res
+        return res.status(200).json({
+            success: true,
+            data:{
+                selectCategory,
+                differentCategories,
+            },
+        })
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
     }
 }
--- models/Course.js
+++ models/Course.js
@@ -41,6 +41,10 @@ const courseSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref:"User",
     required: true,
+  },
+  status:{
+    type: String,
+    enum: ["Draft", "Published"],
   }
 });
 
